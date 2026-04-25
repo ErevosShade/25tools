76ab67afe6dbce7c30590b742fe01b9d370eaad8
ErevosShade <mysilentblade.com>
2026-04-26 01:06:43 +0530

    development ip access removed

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,6 @@ const nextConfig: NextConfig = {
     imageSizes:       [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL:  60 * 60 * 24 * 30,
   },
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://192.168.*.*",
-    "192.168.100.57"   // optional: local network access
-  ],
 
   serverExternalPackages: [],
 
